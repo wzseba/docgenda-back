cb8ff77e1109a36bfa0a129b7f6ada377374cc5c
wzseba <wzseba@gmail.com>
2023-04-02 23:44:17 -0300

    se agregaron routes medicosRouter

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
 import pacientesRouter from './routes/pacientes.routes.js';
+import medicosRouter from './routes/medicos.routes.js';
 const app = express();
 
 //middlewares
 app.use(express.json());//para leer por req.body
 app.use(morgan('dev'));//para ver los status de peticiones
 
-
+//routes
 app.use(pacientesRouter);
+app.use(medicosRouter);
 
 export default app;
