0bc6d70e7e7fba579497f9a489eded8d5ea76862
wzseba <wzseba@gmail.com>
2023-04-16 00:52:46 -0300

    add: cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 import pacientesRouter from './routes/pacientes.routes.js';
 import medicosRouter from './routes/medicos.routes.js';
@@ -7,6 +8,7 @@ const app = express();
 //middlewares
 app.use(express.json());//para leer por req.body
 app.use(morgan('dev'));//para ver los status de peticiones
+app.use(cors());//para aceptar peticiones del front se puede configurar puertos
 
 //routes
 app.use(pacientesRouter);
